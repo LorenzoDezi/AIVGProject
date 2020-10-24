3d8ef64f817f4c8e3789c1d57f5f1c3497289333
Lorenzo Dezi <dezio95.ld@gmail.com>
2020-10-24 15:09:48 +0200

    CameraFollow

---
+++ Assets/Scripts/CameraFollowBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Transform transformToFollow;
+
+    private new Transform transform;
+    private Vector3 currentPosition;
+
+    private void Awake() {
+        transform = GetComponent<Transform>();
+        currentPosition = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 positionToFollow = transformToFollow.position;
+        currentPosition.x = positionToFollow.x;
+        currentPosition.y = positionToFollow.y;
+        transform.position = currentPosition;
+    }
+}
