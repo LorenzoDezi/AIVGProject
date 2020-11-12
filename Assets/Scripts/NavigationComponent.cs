554490c0a6bef7e26c34497af601531d1833145a
Lorenzo Dezi <dezio95.ld@gmail.com>
2020-11-12 09:42:55 +0100

    Navigation Component for the enemy

---
+++ Assets/Scripts/NavigationComponent.cs
@@ -0,0 +1,63 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NavigationComponent : MonoBehaviour {
+
+    private Transform targetPosition;
+    private Seeker seeker;
+    private CharacterController controller;
+    private Path path;
+
+    private float waypointDistTreshold = 2;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath;
+
+    public UnityEvent PathCompleted;
+
+    private void Start() {
+        seeker = GetComponent<Seeker>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void MoveTo(Transform targetPosition) {
+        this.targetPosition = targetPosition;
+        seeker.StartPath(transform.position, targetPosition.position);
+    }
+
+    private void OnEnable() {
+        seeker.pathCallback += OnPathCalculated;
+    }
+
+    private void OnDisable() {
+        seeker.pathCallback -= OnPathCalculated;
+    }
+
+    private void OnPathCalculated(Path p) {
+        Debug.Log("A path was calculated. Did it fail with an error? " + p.error);
+        if (p.error)
+            return;
+        path = p;
+        currentWaypoint = 0;
+    }
+
+    private void Update() {
+        if (path == null)
+            return;
+        float sqrDistanceTreshold = waypointDistTreshold * waypointDistTreshold;
+        float sqrDistanceToWaypoint = Vector3.SqrMagnitude(path.vectorPath[currentWaypoint] - transform.position);
+        while (sqrDistanceToWaypoint < sqrDistanceTreshold) {
+            if (currentWaypoint + 1 < path.vectorPath.Count) {
+                currentWaypoint++;
+                sqrDistanceToWaypoint = Vector3.SqrMagnitude(path.vectorPath[currentWaypoint] - transform.position);
+            } else {
+                PathCompleted.Invoke();
+                break;
+            }
+        }
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        controller.Move(dir);
+    }
+}
