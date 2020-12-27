da349b9183470cd6aaff153e004f547863ca0f4c
Lorenzo Dezi <dezio95.ld@gmail.com>
2020-12-27 11:57:37 +0100

    HealthStation refill ended sprite

--- Assets/Scripts/HealthStation.cs
+++ Assets/Scripts/HealthStation.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class HealthStation : MonoBehaviour {
+
+    [SerializeField]
+    private SpriteRenderer noRefillSprite;
+    private SpriteRenderer sprite;
+
     [SerializeField]
     private int maxRefills = 5;
     private int currRefills;
@@ -17,18 +22,30 @@ public class HealthStation : MonoBehaviour {
     public UnityEvent RefillEndEvent { get; } = new UnityEvent();
 
     private void Awake() {
+
         currRefills = maxRefills;
+
         Transform = GetComponent<Transform>();
+        sprite = GetComponent<SpriteRenderer>();
+        noRefillSprite.enabled = false;
     }
 
     public bool UseRefill() {
         if(currRefills > 0) {
             currRefills--;
             if (currRefills == 0)
-                RefillEndEvent.Invoke();
+                RefillEnded();
             return true;
         }
         return false;
+    }
+
+    private void RefillEnded() {
+        RefillEndEvent.Invoke();
+        var color = sprite.color;
+        color.a = color.a/3f;
+        sprite.color = color;
+        noRefillSprite.enabled = true;
     }
 }
 
